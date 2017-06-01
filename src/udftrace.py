9681071ea0397563ffccd6484579c9aecfd59c7c
dinggul <dinggul@kaist.ac.kr>
2017-06-01 21:48:01 +0900

    Add default debug information for test1 binary

--- src/udftrace.py
+++ src/udftrace.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python2
-import yaml
+from yaml import load as y_load, dump as y_dump
+import sys
+
+def main():
+    if len(sys.argv) < 2:
+        inp_name = 'input.yml'
+    else:
+        inp_name = sys.argv[1]
+
+    with open(inp_name, 'r') as f:
+        debug_info = y_load(f.read())
+
+    return
+
+if __name__ == '__main__':
+    main()
