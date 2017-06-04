c61cb183a85e75e59702996837b6be8ac81a6828
blukat29 <yunjong@kaist.ac.kr>
2017-06-05 03:44:54 +0900

    Define trace spec structures

--- src/udftrace.cpp
+++ src/udftrace.cpp
@@ -1,7 +1,28 @@
 #include <fstream>
 #include <iostream>
+#include <stdlib.h>
 
 #include "pin.H"
+#include <stdint.h>
+
+typedef struct {
+    uint8_t kind;       // argument type.
+    uint8_t location;   // argument location (stack or register).
+    uint16_t stack_ofs; // used for stack arguments.
+    uint32_t mem_size;  // used for memory arguments.
+} UDFType;
+
+typedef struct {
+    uint64_t addr;
+    uint64_t nargs;
+    UDFType ret_ty;
+    UDFType arg_ty[0]; // 'nargs' elements.
+} UDFFunc;
+
+typedef struct {
+    uint64_t nfuncs;
+    UDFFunc funcs[0]; // 'nfuncs' elements.
+} UDFSpec;
 
 using namespace std;
 
@@ -12,7 +33,14 @@ INT32 Usage() {
 
 VOID Routine(RTN rtn, VOID* v)
 {
-    cout << "Routine " << RTN_Address(rtn) << endl;
+    UDFSpec* spec = (UDFSpec*)v;
+    ADDRINT addr = RTN_Address(rtn);
+    for (unsigned int i = 0; i < spec->nfuncs; i ++) {
+        if (spec->funcs[i].addr == addr) {
+            cout << "Instrumenting " << addr << endl;
+            break;
+        }
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -23,8 +51,15 @@ int main(int argc, char *argv[]) {
     cout << hex;
     cout.setf(ios::showbase);
 
-    RTN_AddInstrumentFunction(Routine, NULL);
+    int nfuncs = 1;
+    UDFSpec* spec = (UDFSpec*) malloc(sizeof(UDFSpec) + nfuncs * sizeof(UDFFunc));
+    spec->nfuncs = nfuncs;
+    spec->funcs[0].addr = 0x400526; // test2 / fibo()
+    spec->funcs[0].nargs = 1;
 
+    RTN_AddInstrumentFunction(Routine, spec);
+
+    PIN_InitSymbols();
     PIN_StartProgram();
     return 0;
 }
