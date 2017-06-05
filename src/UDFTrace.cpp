add0c25c0544591af68f00d0c70856884bd96559
blukat29 <yunjong@kaist.ac.kr>
2017-06-05 12:29:58 +0900

    Split off UDFSpec definitions

--- src/UDFTrace.cpp
+++ src/UDFTrace.cpp
@@ -3,26 +3,7 @@
 #include <stdlib.h>
 
 #include "pin.H"
-#include <stdint.h>
-
-typedef struct {
-    uint8_t kind;       // argument type.
-    uint8_t location;   // argument location (stack or register).
-    uint16_t stack_ofs; // used for stack arguments.
-    uint32_t mem_size;  // used for memory arguments.
-} UDFType;
-
-typedef struct {
-    uint64_t addr;
-    uint64_t nargs;
-    UDFType ret_ty;
-    UDFType arg_ty[0]; // 'nargs' elements.
-} UDFFunc;
-
-typedef struct {
-    uint64_t nfuncs;
-    UDFFunc funcs[0]; // 'nfuncs' elements.
-} UDFSpec;
+#include "spec.h"
 
 using namespace std;
 
